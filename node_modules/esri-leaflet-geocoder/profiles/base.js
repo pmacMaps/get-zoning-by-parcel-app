aae4c9b02693b724c67faaab34686925cb569f1c
Patrick McKinney <pnmcartography@gmail.com>
2018-10-26 12:35:47 -0400

    add gitignore. remove node_modules dir from vc

--- node_modules/esri-leaflet-geocoder/profiles/base.js
+++
@@ -1,6 +0,0 @@
-import config from '../node_modules/esri-leaflet/profiles/base.js';
-
-config.entry = 'src/EsriLeafletGeocoding.js';
-config.moduleName = 'L.esri.Geocoding';
-
-export default config;
--- node_modules/esri-leaflet-geocoder/profiles/debug.js
+++
@@ -1,6 +0,0 @@
-import config from './base.js';
-
-config.dest = 'dist/esri-leaflet-geocoder-debug.js';
-config.sourceMap = 'inline';
-
-export default config;
